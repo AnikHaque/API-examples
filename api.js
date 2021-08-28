87dd5a15ed5e56dfcfa4ed5c2f675042f4a083df
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-08-28 17:02:38 +0600

    added

---
+++ api.js
@@ -0,0 +1,18 @@
+// json (JavaScript Object Notation)
+
+const student = {
+
+name: 'Ekramul',
+id: 15,
+country: 'Bangladesh'
+
+}
+
+const stringified = JSON.stringify(student);
+
+console.log(stringified);
+
+// normal console 
+console.log(student);
+
+
